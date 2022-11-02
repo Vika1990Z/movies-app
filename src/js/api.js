86a16cec92e07de36c70461ae410a8cbca4d1b00
Vika1990Z <53933994+Vika1990Z@users.noreply.github.com>
2022-11-02 21:07:04 +0200

    part 2

--- src/js/api.js
+++ src/js/api.js
@@ -1,18 +1,46 @@
 import { generateURL } from "./utils";
-import {renderMovies, renderError} from './templates';
+import {renderMovies, renderError, renderMovieDetails} from './templates';
 
 export const getMovies = (path, selector, text) => {
     fetch(generateURL(path))
       .then(res => res.json())
       .then(data => {
-        console.log('results', data.results);
-  
         const root = document.querySelector(selector);
-  
-        if(data.status_message && data.status_code) {
+        if(data.success === false) {
           root.innerHTML += renderError({ status_code: data.status_code, status_message: data.status_message }, text);
         } else {
           root.innerHTML += renderMovies(data.results, text);
         }
       });
-  }
+}
+
+
+export const getMovieDetails = (path, selector) => {
+  fetch(generateURL(path))
+    .then(res => res.json())
+    .then(data => {
+
+      console.log(data);
+      const root = document.querySelector(selector);
+
+      if(data.success === false) {
+        root.innerHTML = renderError({ status_code: data.status_code, status_message: data.status_message }, "Can't render movie detailes");
+      } else {
+        root.innerHTML = renderMovieDetails(data);
+      }
+    });
+}
+
+
+export const searchMovies = (path, selector, text, matches) => {
+  fetch(`${generateURL(path)}&query=${matches}`)
+    .then(res => res.json())
+    .then(data => {
+      const root = document.querySelector(selector);
+      if(data.success === false) {
+        root.innerHTML += renderError({ status_code: data.status_code, status_message: data.status_message }, text);
+      } else {
+        root.innerHTML += renderMovies(data.results, text);
+      }
+    });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,38 @@
+import {getMovies, getMovieDetails, searchMovies} from "./api";
+import { generateHash } from "./utils";
 
-import {getMovies} from "./api";
+const root = document.querySelector(".container-main");
+const input = document.querySelector('input');
 
-getMovies('movie/popular', ".container-main", "Today is popular...")
+checkUrl();
+
+input.addEventListener('change', (e) => {
+    root.innerHTML = '';
+    location.hash = input.value;
+})
+
+window.addEventListener('hashchange', (e) => {
+    checkUrl();
+})
 
 
-getMovies('movie/top_rated', ".container-main", "Top-rated movies...")    
+function checkUrl () {
+    const hash = location.hash;
+    const [hashName] = hash.split('=');
+
+    if (hashName === '#movieId') {
+        getMovieDetails(`movie/${generateHash(hash)}`, ".container-main");  
+    } else if (hashName != '') {
+        searchMovies(`search/movie`, ".container-main", "Found matches...", hashName.slice(1))
+    } else {
+        root.innerHTML = '';
+        getMovies('movie/popular', ".container-main", "Today is popular...");
+        getMovies('movie/top_rated', ".container-main", "Top-rated movies..."); 
+        getMovies('movie/upcoming', ".container-main", "Upcoming...")  ;
+    }
+  }
 
 
-getMovies('movie/upcoming', ".container-main", "Upcoming...")  
+
+
+
